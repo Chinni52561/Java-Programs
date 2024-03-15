4371a8064db102806f7db5b0f277bf37a92ca32c
Leela Pravallika <136470623+Chinni52561@users.noreply.github.com>
2024-03-15 19:07:04 +0530

    Add files via upload
    Day1

---
+++ SalesForce2.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SalesForce2 {
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com");
+	}
+	
+
+}
